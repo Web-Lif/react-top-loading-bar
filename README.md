80f6f99e7887710e69a0a531af05808f11b3089e
robothot <854363956@qq.com>
2022-04-22 22:54:20 +0800

    feat: 发布

--- README.md
+++ README.md
@@ -1,33 +1,17 @@
 # react-top-loading-bar
 
->
-
-[![NPM](https://img.shields.io/npm/v/react-top-loading-bar.svg)](https://www.npmjs.com/package/react-top-loading-bar) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
-[![npm download][download-image]][download-url]
-
-[download-image]: https://img.shields.io/npm/dm/react-top-loading-bar.svg
-[download-url]: https://npmjs.org/package/react-top-loading-bar
-
-[![react-top-loading-bar](https://nodei.co/npm/react-top-loading-bar.png)](https://npmjs.org/package/react-top-loading-bar)
-
 ## Install
 
-- using npm
+-   using npm
 
 ```bash
-npm install --save react-top-loading-bar
+npm install --save @weblif/react-top-loading-bar
 ```
 
-- using yarn
+-   using yarn
 
 ```bash
-yarn add react-top-loading-bar
-```
-
-- CDN
-
-```
-https://unpkg.com/react-top-loading-bar
+yarn add @weblif/react-top-loading-bar
 ```
 
 ## Usage
@@ -36,24 +20,24 @@ https://unpkg.com/react-top-loading-bar
 
 ```jsx
 import React, { useRef } from 'react'
-import LoadingBar from 'react-top-loading-bar'
+import LoadingBar from '@weblif/react-top-loading-bar'
 
 const App = () => {
-  const ref = useRef(null)
+    const ref = useRef(null)
 
-  return (
-    <div>
-      <LoadingBar color='#f11946' ref={ref} />
-      <button onClick={() => ref.current.continuousStart()}>
-        Start Continuous Loading Bar
-      </button>
-      <button onClick={() => ref.current.staticStart()}>
-        Start Static Loading Bar
-      </button>
-      <button onClick={() => ref.current.complete()}>Complete</button>
-      <br />
-    </div>
-  )
+    return (
+        <div>
+            <LoadingBar color="#f11946" ref={ref} />
+            <button onClick={() => ref.current.continuousStart()}>
+                Start Continuous Loading Bar
+            </button>
+            <button onClick={() => ref.current.staticStart()}>
+                Start Static Loading Bar
+            </button>
+            <button onClick={() => ref.current.complete()}>Complete</button>
+            <br />
+        </div>
+    )
 }
 
 export default App
@@ -66,21 +50,21 @@ import React, { useState } from 'react'
 import LoadingBar from 'react-top-loading-bar'
 
 const App = () => {
-  const [progress, setProgress] = useState(0)
+    const [progress, setProgress] = useState(0)
 
-  return (
-    <div>
-      <LoadingBar
-        color='#f11946'
-        progress={progress}
-        onLoaderFinished={() => setProgress(0)}
-      />
-      <button onClick={() => setProgress(progress + 10)}>Add 10%</button>
-      <button onClick={() => setProgress(progress + 20)}>Add 20%</button>
-      <button onClick={() => setProgress(100)}>Complete</button>
-      <br />
-    </div>
-  )
+    return (
+        <div>
+            <LoadingBar
+                color="#f11946"
+                progress={progress}
+                onLoaderFinished={() => setProgress(0)}
+            />
+            <button onClick={() => setProgress(progress + 10)}>Add 10%</button>
+            <button onClick={() => setProgress(progress + 20)}>Add 20%</button>
+            <button onClick={() => setProgress(100)}>Complete</button>
+            <br />
+        </div>
+    )
 }
 
 export default App
@@ -102,33 +86,33 @@ export default App
 
 ## Properties
 
-| Property         | Type     | Default | Description                                                                                                                       |
-| :--------------- | :------- | :------ | :-------------------------------------------------------------------------------------------------------------------------------- |
-| progress         | Number   | `0`     | The progress/width indicator, progress prop varies from `0` to `100`.                                                             |
-| color            | String   | `red`   | The color of the loading bar, color take values like css property `background:` do, for example `red`, `#000` `rgb(255,0,0)` etc. |
-| shadow           | Boolean  | `true`  | Enables / Disables shadow underneath the loader.                                                                                  |
-| height           | Number   | `2`     | The height of the loading bar in pixels.                                                                                          |
-| background       | String   | `3`     | The loader parent background color.                                                                                               |
-| style       | CSSProperties   |    | The style attribute to loader's div                                                                                             |
-| containerStyle       | CSSProperties   |    | The style attribute to loader's container                                                                                            |
-| shadowStyle       | CSSProperties   |    | The style attribute to loader's shadow                                                                                          |
-| transitionTime   | Number   | `300`   | Fade transition time in miliseconds.                                                                                              |
-| loaderSpeed      | Number   | `500`   | Loader transition speed in miliseconds.                                                                                           |
-| waitingTime      | Number   | `1000`  | The delay we wait when bar reaches 100% before we proceed fading the loader out.                                                  |
-| className        | String   |         | You can provide a class you'd like to add to the loading bar to add some styles to it                                             |
-| containerClassName        | String   |         | You can provide a class you'd like to add to the loading bar container to add some css styles                                         |
-| onLoaderFinished | Function |         | This is called when the loading bar completes, reaches 100% of his width.                                                         |
+| Property           | Type          | Default | Description                                                                                                                       |
+| :----------------- | :------------ | :------ | :-------------------------------------------------------------------------------------------------------------------------------- |
+| progress           | Number        | `0`     | The progress/width indicator, progress prop varies from `0` to `100`.                                                             |
+| color              | String        | `red`   | The color of the loading bar, color take values like css property `background:` do, for example `red`, `#000` `rgb(255,0,0)` etc. |
+| shadow             | Boolean       | `true`  | Enables / Disables shadow underneath the loader.                                                                                  |
+| height             | Number        | `2`     | The height of the loading bar in pixels.                                                                                          |
+| background         | String        | `3`     | The loader parent background color.                                                                                               |
+| style              | CSSProperties |         | The style attribute to loader's div                                                                                               |
+| containerStyle     | CSSProperties |         | The style attribute to loader's container                                                                                         |
+| shadowStyle        | CSSProperties |         | The style attribute to loader's shadow                                                                                            |
+| transitionTime     | Number        | `300`   | Fade transition time in miliseconds.                                                                                              |
+| loaderSpeed        | Number        | `500`   | Loader transition speed in miliseconds.                                                                                           |
+| waitingTime        | Number        | `1000`  | The delay we wait when bar reaches 100% before we proceed fading the loader out.                                                  |
+| className          | String        |         | You can provide a class you'd like to add to the loading bar to add some styles to it                                             |
+| containerClassName | String        |         | You can provide a class you'd like to add to the loading bar container to add some css styles                                     |
+| onLoaderFinished   | Function      |         | This is called when the loading bar completes, reaches 100% of his width.                                                         |
 
 ## Projects using react-top-loading-bar
 
-- [React Movies](https://github.com/klendi/react-movies)
-- [WCYB](https://github.com/klendi/wcyb)
+-   [React Movies](https://github.com/klendi/react-movies)
+-   [WCYB](https://github.com/klendi/wcyb)
 
 Add your own project. Make a PR
 
 ## Migrate from V.1
 
-- Replace onRef prop with 'ref', assign it to a react ref. Access methods with reactRef.current.xxx
+-   Replace onRef prop with 'ref', assign it to a react ref. Access methods with reactRef.current.xxx
 
 ## Code Style
 
--- example/src/App.js
+++ example/src/App.js
@@ -1,184 +1,188 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from 'react'
 
-import LoadingBar from "react-top-loading-bar";
-import "./index.css";
-import { changeColor } from "./changeColor";
-import Highlight from "react-highlight";
+import LoadingBar from 'react-top-loading-bar'
+import './index.css'
+import { changeColor } from './changeColor'
+import Highlight from 'react-highlight'
 
 const App = () => {
-  const [progress, setProgress] = useState(0);
-  const [barColor, setBarColor] = useState("#f11946");
-  const [buttonsColor, setButtonsColor] = useState("red");
-  const ref = useRef(null);
-  const [usingRef, setUsingRef] = useState(false);
+    const [progress, setProgress] = useState(0)
+    const [barColor, setBarColor] = useState('#f11946')
+    const [buttonsColor, setButtonsColor] = useState('red')
+    const ref = useRef(null)
+    const [usingRef, setUsingRef] = useState(false)
 
-  const saveToClipboard = (text) => {
-    navigator.clipboard.writeText(text).then(() => {
-      window.alert("Copied To Clipboard");
-    });
-  };
-
-  const changeMode = (refMode) => {
-    if (refMode) {
-      setProgress(0);
+    const saveToClipboard = (text) => {
+        navigator.clipboard.writeText(text).then(() => {
+            window.alert('Copied To Clipboard')
+        })
     }
 
-    setUsingRef(refMode);
-  };
+    const changeMode = (refMode) => {
+        if (refMode) {
+            setProgress(0)
+        }
 
-  return (
-    <div>
-      {usingRef ? (
-        <LoadingBar color={barColor} ref={ref} shadow={true} />
-      ) : (
-        <LoadingBar
-          color={barColor}
-          progress={progress}
-          onLoaderFinished={() => setProgress(0)}
-        />
-      )}
+        setUsingRef(refMode)
+    }
 
-      <div className="text-container">
-        <h1 className="header">react-top-loading-bar</h1>
-        <div className="inline">
-          <code
-            className="package-install-text"
-            onClick={() => saveToClipboard("npm i react-top-loading-bar")}
-          >
-            npm i react-top-loading-bar
-          </code>
-          <br />
-          or
-          <br />
-          <code
-            className="package-install-text"
-            onClick={() => saveToClipboard("yarn add react-top-loading-bar")}
-          >
-            yarn add react-top-loading-bar
-          </code>
-        </div>
-      </div>
-      <div className="buttons-group">
-        <Highlight language="javascript" className="code-highlighter">
-          {usingRef
-            ? `const ref = useRef(null);\n<LoadingBar color={barColor} ref={ref} />\nref.current.continuousStart()`
-            : `const [progress,setProgress] = useState(0);\n<LoadingBar color={barColor} progress={progress}
-    onLoaderFinished={() => setProgress(0)} />`}
-        </Highlight>
-        <br />
-        {usingRef ? (
-          <div>
-            <button
-              className={"btn " + buttonsColor}
-              onClick={() => ref.current.continuousStart()}
-            >
-              Start Continuous Loading Bar
-            </button>
-            <button
-              className={"btn " + buttonsColor}
-              onClick={() => ref.current.staticStart()}
-            >
-              Start Static Loading Bar
-            </button>
-            <button
-              className={"btn " + buttonsColor}
-              onClick={() => ref.current.complete()}
-            >
-              Complete
-            </button>
-            <br />
-          </div>
-        ) : (
-          <div>
-            <button
-              className={"btn " + buttonsColor}
-              onClick={() => setProgress(progress + 10)}
-            >
-              Add 10%
-            </button>
-            <button
-              className={"btn " + buttonsColor}
-              onClick={() => setProgress(progress + 30)}
-            >
-              Add 30%
-            </button>
-            <button
-              className={"btn " + buttonsColor}
-              onClick={() => setProgress(progress + 50)}
-            >
-              Add 50%
-            </button>
-
-            <br />
-          </div>
-        )}
-
-        <button
-          className={"btn " + buttonsColor}
-          onClick={() => {
-            const colors = changeColor(buttonsColor);
-            setBarColor(colors.barColor);
-            setButtonsColor(colors.color);
-          }}
-        >
-          Change Color
-        </button>
-        <button
-          className={"btn " + buttonsColor}
-          onClick={() => changeMode(!usingRef)}
-        >
-          Change to {usingRef ? "State" : "Refs"} Mode
-        </button>
-        <a
-          className={"btn " + buttonsColor}
-          target="_blank"
-          rel="noopener noreferrer"
-          href={
-            usingRef
-              ? "https://github.com/klendi/react-top-loading-bar/blob/master/example/examples/exampleWithRef.js"
-              : "https://github.com/klendi/react-top-loading-bar/blob/master/example/examples/exampleWithState.js"
-          }
-        >
-          Example
-        </a>
-        <br />
-        <br />
-        <div className="github-buttons">
-          <a
-            className="github-button"
-            href="https://github.com/klendi/react-top-loading-bar"
-            data-color-scheme="no-preference: light; light: light; dark: dark;"
-            data-size="large"
-            data-show-count="true"
-            aria-label="Star klendi/react-top-loading-bar on GitHub"
-          >
-            Star
-          </a>{" "}
-          <a
-            className="github-button"
-            href="https://github.com/klendi"
-            data-color-scheme="no-preference: light; light: light; dark: dark;"
-            data-size="large"
-            data-show-count="true"
-            aria-label="Follow @klendi on GitHub"
-          >
-            Follow @klendi
-          </a>
-        </div>
-        <br />
+    return (
         <div>
-          Made with ❤️ by{" "}
-          <a
-            href="https://klendi.dev"
-            style={{ color: barColor }}
-            target="_blank"
-          >
-            Klendi Gocci
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-};
+            {usingRef ? (
+                <LoadingBar color={barColor} ref={ref} shadow={true} />
+            ) : (
+                <LoadingBar
+                    color={barColor}
+                    progress={progress}
+                    onLoaderFinished={() => setProgress(0)}
+                />
+            )}
 
-export default App;
+            <div className="text-container">
+                <h1 className="header">react-top-loading-bar</h1>
+                <div className="inline">
+                    <code
+                        className="package-install-text"
+                        onClick={() =>
+                            saveToClipboard('npm i react-top-loading-bar')
+                        }
+                    >
+                        npm i @weblif/react-top-loading-bar
+                    </code>
+                    <br />
+                    or
+                    <br />
+                    <code
+                        className="package-install-text"
+                        onClick={() =>
+                            saveToClipboard('yarn add react-top-loading-bar')
+                        }
+                    >
+                        yarn add @weblif/react-top-loading-bar
+                    </code>
+                </div>
+            </div>
+            <div className="buttons-group">
+                <Highlight language="javascript" className="code-highlighter">
+                    {usingRef
+                        ? `const ref = useRef(null);\n<LoadingBar color={barColor} ref={ref} />\nref.current.continuousStart()`
+                        : `const [progress,setProgress] = useState(0);\n<LoadingBar color={barColor} progress={progress}
+    onLoaderFinished={() => setProgress(0)} />`}
+                </Highlight>
+                <br />
+                {usingRef ? (
+                    <div>
+                        <button
+                            className={'btn ' + buttonsColor}
+                            onClick={() => ref.current.continuousStart()}
+                        >
+                            Start Continuous Loading Bar
+                        </button>
+                        <button
+                            className={'btn ' + buttonsColor}
+                            onClick={() => ref.current.staticStart()}
+                        >
+                            Start Static Loading Bar
+                        </button>
+                        <button
+                            className={'btn ' + buttonsColor}
+                            onClick={() => ref.current.complete()}
+                        >
+                            Complete
+                        </button>
+                        <br />
+                    </div>
+                ) : (
+                    <div>
+                        <button
+                            className={'btn ' + buttonsColor}
+                            onClick={() => setProgress(progress + 10)}
+                        >
+                            Add 10%
+                        </button>
+                        <button
+                            className={'btn ' + buttonsColor}
+                            onClick={() => setProgress(progress + 30)}
+                        >
+                            Add 30%
+                        </button>
+                        <button
+                            className={'btn ' + buttonsColor}
+                            onClick={() => setProgress(progress + 50)}
+                        >
+                            Add 50%
+                        </button>
+
+                        <br />
+                    </div>
+                )}
+
+                <button
+                    className={'btn ' + buttonsColor}
+                    onClick={() => {
+                        const colors = changeColor(buttonsColor)
+                        setBarColor(colors.barColor)
+                        setButtonsColor(colors.color)
+                    }}
+                >
+                    Change Color
+                </button>
+                <button
+                    className={'btn ' + buttonsColor}
+                    onClick={() => changeMode(!usingRef)}
+                >
+                    Change to {usingRef ? 'State' : 'Refs'} Mode
+                </button>
+                <a
+                    className={'btn ' + buttonsColor}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={
+                        usingRef
+                            ? 'https://github.com/klendi/react-top-loading-bar/blob/master/example/examples/exampleWithRef.js'
+                            : 'https://github.com/klendi/react-top-loading-bar/blob/master/example/examples/exampleWithState.js'
+                    }
+                >
+                    Example
+                </a>
+                <br />
+                <br />
+                <div className="github-buttons">
+                    <a
+                        className="github-button"
+                        href="https://github.com/klendi/react-top-loading-bar"
+                        data-color-scheme="no-preference: light; light: light; dark: dark;"
+                        data-size="large"
+                        data-show-count="true"
+                        aria-label="Star klendi/react-top-loading-bar on GitHub"
+                    >
+                        Star
+                    </a>{' '}
+                    <a
+                        className="github-button"
+                        href="https://github.com/klendi"
+                        data-color-scheme="no-preference: light; light: light; dark: dark;"
+                        data-size="large"
+                        data-show-count="true"
+                        aria-label="Follow @klendi on GitHub"
+                    >
+                        Follow @klendi
+                    </a>
+                </div>
+                <br />
+                <div>
+                    Made with ❤️ by{' '}
+                    <a
+                        href="https://klendi.dev"
+                        style={{ color: barColor }}
+                        target="_blank"
+                    >
+                        Klendi Gocci
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
